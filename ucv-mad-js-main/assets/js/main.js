fb3010d81bb38e5d591b903a57943ccbca1fd83f
k43ru <danielparrariera7@gmail.com>
2024-10-10 14:27:45 +0200

    Start

--- ucv-mad-js-main/assets/js/main.js
+++ ucv-mad-js-main/assets/js/main.js
@@ -2,7 +2,7 @@
 /* Ignoramos esta parte por el momento */
 /*****************************************/
 let intervalId = window.setInterval(function () {
-    console.log("Clock isn't working yet!");
+    //console.log("Clock isn't working yet!");
 }, 10000);
 
 function stopClock() { 
@@ -12,4 +12,37 @@ function stopClock() {
 
 window.onload = function () { 
     console.log("Hello, world!");
+    console.log(Math.random());
+
+
+
+const miReloj = document.getElementById('clock');
+
+
+console.dir(miReloj);
+
+
+miReloj.innerText = "Hola,hola!";
+
+const headers = document.getElementsByTagName('h1');
+
+console.dir(headers[0]);
+
+const divs = document.getElementsByTagName('div');
+
+console.dir(divs);
+
+}
+
+
+function randInt(min,max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function changeEmojiColor(elem){
+    const r = randInt(0, 255);
+    const g = randInt(0, 255);
+    const b = randInt(0, 255);
+    elem.style.fill = 'rgb(' + r + ',' + g +',' + b +')';
 }
