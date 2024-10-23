4fba8c1d75601b6c93e0a7b97a4d38959a5f5b20
k43ru <danielparrariera7@gmail.com>
2024-10-23 12:19:02 +0200

    1

--- ucv-mad-js-main/assets/js/main.js
+++ ucv-mad-js-main/assets/js/main.js
@@ -72,12 +72,8 @@ function xd (elem){
     document.getElementById("gay").style.fill = "grey";
 }
 function xdd(elem){
-    document.getElementById("gay").style.backgroundImage = "url('ojos.jpg')";
+    document.getElementById("gay").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-2-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm4.646 6.24v.07H5.375v-.064c0-1.213.879-2.402 2.637-2.402 1.582 0 2.613.949 2.613 2.215 0 1.002-.6 1.667-1.287 2.43l-.096.107-1.974 2.22v.077h3.498V12H5.422v-.832l2.97-3.293c.434-.475.903-1.008.903-1.705 0-.744-.557-1.236-1.313-1.236-.843 0-1.336.615-1.336 1.306"/> </svg>';
 }
-
-
-
-
 
 
 
@@ -93,8 +89,11 @@ function b2 (elem) {
 function changeImage(elem){
 
     const miVar = document.getElementsByTagName("main"); 
-    miVar[0].style.backgroundImage = "url('ucv-mad-js-main/assets/img/moonlit-lake.jpg')";
+    miVar[0].style.backgroundImage = "url('./assets/img/moonlit-lake.jpg')";
+
 }
+
+
 
 
 
